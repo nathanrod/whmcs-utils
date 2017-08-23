b6155851b84cff3da36b4b45f2c44ae87506a8b3
Nathan <nathan@stkfactory>
2017-08-23 12:53:40 +0200

    bug fixes

--- create_addon.sh
+++ create_addon.sh
@@ -5,7 +5,12 @@ addon_name=$2
 
 case $addon_type in
     provisioning )
-        git clone https://github.com/WHMCS/sample-provisioning-module.git $addon_name
+        rm -rf $addon_name
+        rm -rf /tmp/$addon_name
+        git clone https://github.com/WHMCS/sample-provisioning-module.git /tmp/$addon_name
+        cp -r /tmp/$addon_name/modules/servers/provisioningmodule $addon_name
+        mv $addon_name/provisioningmodule.php $addon_name/$addon_name.php       
+        sed -i -- "s/provisioningmodule_/${addon_name}_/g" $addon_name/$addon_name.php
     ;;
     gateway )
         git clone https://github.com/WHMCS/sample-gateway-module.git $addon_name
@@ -14,7 +19,13 @@ case $addon_type in
         git clone https://github.com/WHMCS/sample-merchant-gateway.git $addon_name
     ;;
     addon )
-        git clone https://github.com/WHMCS/sample-registrar-module.git $addon_name
+        rm -rf $addon_name
+        rm -rf /tmp/$addon_name
+        git clone https://github.com/WHMCS/sample-addon-module.git /tmp/$addon_name
+        cp -r /tmp/$addon_name/modules/addons/addonmodule $addon_name
+        mv $addon_name/addonmodule.php $addon_name/$addon_name.php
+        sed -i -- "s/addonmodule_/${addon_name}_/g" $addon_name/$addon_name.php
+
     ;;
     registrar )
         git clone https://github.com/WHMCS/sample-registrar-module.git $addon_name
